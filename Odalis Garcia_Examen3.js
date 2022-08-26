cae450f2461e1db3a6b1cec0944b5fdf4d1ae8d9
Odamg <godalis64@gmail.com>
2022-08-25 21:43:42 -0500

    25/08/2022

--- Odalis Garcia_Examen3.js
+++ Odalis Garcia_Examen3.js
@@ -1,17 +1,17 @@
-matrz1 = [3, 2, 1, 1, 2, 3, 2, 3, 1]
-matrz2 = [1, 1, 2, 2, 1, 1, 1, 2, 1]
+mtrz1 = [3, 2, 1, 1, 2, 3, 2, 3, 1]
+mtrz2 = [1, 1, 2, 2, 1, 1, 1, 2, 1]
 
-console.log("Arreglo 1: ", matrz1 )
+console.log("Arreglo 1: ", mtrz1 )
 console.log("--------------------------")
 
 
-console.log("Arreglo 2: ", matrz2 )
+console.log("Arreglo 2: ", mtrz2 )
 console.log("--------------------------")
 
 
-for (let i in matrz1){
-  console.log(matrz1[i] + " + " + matrz2[i] + " = " + (matrz1[i]+matrz2[i]))
-  console.log(matrz1[i] + " * " + matrz2[i] + " = " + (matrz1[i]*matrz2[i]))
+for (let i in mtrz1){
+  console.log(mtrz1[i] + " + " + mtrz2[i] + " = " + (mtrz1[i]+mtrz2[i]))
+  console.log(mtrz1[i] + " * " + mtrz2[i] + " = " + (mtrz1[i]*mtrz2[i]))
   console.log("--------------------------")
 }
 
