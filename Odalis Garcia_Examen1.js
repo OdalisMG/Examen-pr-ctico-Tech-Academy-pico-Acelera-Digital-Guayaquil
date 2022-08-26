5224df442f2fb717bf060eec4574cd95bae05a65
Odamg <godalis64@gmail.com>
2022-08-25 21:41:27 -0500

    25/8/2022

--- Odalis Garcia_Examen1.js
+++ Odalis Garcia_Examen1.js
@@ -1,11 +1,10 @@
-horastrabajadas = 53;
-horasextras = 40 - horastrabajadas;
+horas_trabajadas = 53;
+horas_extras = 40 - horas_trabajadas;
   
 
 if(horas_extras < 0){
-  console.log("El total a pagar por hors laborales normal (40hrs) es de: " + 40 * 265);
-  console.log("El total a pagar por hors Extras es de: " + (-horasextras * 350));
+  console.log("El total a pagar por horas laborales normal (40hrs) es de: " + 40 * 265);
+  console.log("El total a pagar por horas Extras es de: " + (-horas_extras * 350));
 }else{
-  console.log("El total a pagar por hors laborales normal (40hrs) es de: " + horastrabajadas * 265);
+  console.log("El total a pagar por horas laborales normal (40hrs) es de: " + horas_trabajadas * 265);
 }
-    
