f0b5432f294a1956f37b3fbf9d0c551f4734ed10
Odamg <godalis64@gmail.com>
2022-08-25 21:28:57 -0500

    25/8/2022

--- Odalis Garcia_Examen3.js
+++ Odalis Garcia_Examen3.js
@@ -1,5 +1,18 @@
-   mA= [ 3, 2, 1, 1, 2, 3, 2, 3, 1];
-   mB= [1, 1, 2, 2, 1, 1, 1, 2, 1];
-   const mA= matematica.matriz ([[]])
+matrz1 = [3, 2, 1, 1, 2, 3, 2, 3, 1]
+matrz2 = [1, 1, 2, 2, 1, 1, 1, 2, 1]
+
+console.log("Arreglo 1: ", matrz1 )
+console.log("--------------------------")
+
+
+console.log("Arreglo 2: ", matrz2 )
+console.log("--------------------------")
+
+
+for (let i in matrz1){
+  console.log(matrz1[i] + " + " + matrz2[i] + " = " + (matrz1[i]+matrz2[i]))
+  console.log(matrz1[i] + " * " + matrz2[i] + " = " + (matrz1[i]*matrz2[i]))
+  console.log("--------------------------")
+}
 
   
